0a4ac315935593a0c0aa654256fba39f8c371ab7
gopalakrishna <rlgk97016@gmail.com>
2024-03-11 07:08:52 +0000

    Simple Todo App

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,4 +1,6 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+
 import './index.css'
 import TodoItem from '../TodoItem'
 
@@ -38,7 +40,7 @@ const initialTodosList = [
 ]
 
 class SimpleTodos extends Component {
-  state = {TodoList: initialTodosList}
+  state = {TodoList: initialTodosList, inputTodo: ''}
 
   deleteTodo = id => {
     const {TodoList} = this.state
@@ -46,18 +48,98 @@ class SimpleTodos extends Component {
     this.setState({TodoList: filteredTodo})
   }
 
+  onClickSaveTodo = id => {
+    const {inputTodo} = this.state
+    console.log(id)
+
+    this.setState(prevState => ({
+      TodoList: prevState.TodoList.map(eachItem => {
+        if (eachItem.id === id) {
+          return {
+            ...eachItem,
+            title: inputTodo,
+          }
+        }
+        return eachItem
+      }),
+      inputTodo: '',
+    }))
+  }
+
+  onClickEditTodo = (id, title) => {
+    console.log(id)
+
+    this.setState({
+      inputTodo: title,
+    })
+  }
+
+  addTodo = () => {
+    const {inputTodo} = this.state
+
+    const wordListTodo = inputTodo.split(' ')
+    console.log(wordListTodo)
+
+    let num = 1
+
+    wordListTodo.forEach(word => {
+      const repetitionValue = parseInt(word)
+      if (!Number.isNaN(repetitionValue)) {
+        console.log(repetitionValue)
+        num = repetitionValue
+      }
+    })
+
+    const Index = wordListTodo.indexOf(num.toString())
+    if (Index !== -1) {
+      wordListTodo.splice(Index, 1)
+    }
+
+    console.log(wordListTodo)
+
+    for (let i = 0; i < num; i += 1) {
+      const newTodo = {
+        id: uuidv4(),
+        title: wordListTodo.join(' '),
+      }
+      this.setState(prevState => ({
+        TodoList: [...prevState.TodoList, newTodo],
+        inputTodo: '',
+      }))
+    }
+  }
+
+  onChangeTodo = event => {
+    this.setState({inputTodo: event.target.value})
+  }
+
   render() {
-    const {TodoList} = this.state
+    const {TodoList, inputTodo} = this.state
     return (
       <div className="bg_container">
         <div className="todos_container">
           <h1 className="main_heading">Simple Todos</h1>
-          <ul>
+          <div className="input-container">
+            <input
+              type="text"
+              className="input-element"
+              onChange={this.onChangeTodo}
+              value={inputTodo}
+              placeholder="Enter Your Todo Item"
+            />
+            <button type="button" className="add-button" onClick={this.addTodo}>
+              Add
+            </button>
+          </div>
+
+          <ul className="todos-list-container">
             {TodoList.map(eachTodo => (
               <TodoItem
                 todoItem={eachTodo}
                 deleteTodo={this.deleteTodo}
                 key={eachTodo.id}
+                onClickEditTodo={this.onClickEditTodo}
+                onClickSaveTodo={this.onClickSaveTodo}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,17 +1,39 @@
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {todoItem, deleteTodo} = props
+  const [isEditing, toggleEditSaveButton] = useState(true)
+  const {todoItem, deleteTodo, onClickEditTodo, onClickSaveTodo} = props
   const {title, id} = todoItem
   const onDelete = () => {
     deleteTodo(id)
   }
+  const onEdit = () => {
+    toggleEditSaveButton(false)
+    onClickEditTodo(id, title)
+  }
+  const onSave = () => {
+    toggleEditSaveButton(true)
+    onClickSaveTodo(id)
+  }
   return (
-    <li>
-      <p>{title}</p>
-      <button type="button" onClick={onDelete}>
-        Delete
-      </button>
+    <li className="todo-list-item">
+      <p className="todo-title">{title}</p>
+      <div className="buttons-container">
+        {isEditing ? (
+          <button type="button" onClick={onEdit} className="button">
+            Edit
+          </button>
+        ) : (
+          <button type="button" onClick={onSave} className="button">
+            Save
+          </button>
+        )}
+
+        <button type="button" onClick={onDelete} className="button">
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
